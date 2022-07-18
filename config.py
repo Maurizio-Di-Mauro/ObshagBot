5c26f92f3a5a36d047652290e82fbedf3578fcce
Zakaria Beno <zakaria.beno@truesalessoft.com>
2022-07-18 21:43:35 +0400

    Fix environment variables

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+from typing import List
 import os
 
 
 class Config:
     TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
-    TRUSTED_IDS = os.getenv('TRUSTED_IDS')
+    TRUSTED_IDS: List[int] = [int(i) for i in os.getenv('TRUSTED_IDS').split(" ")]
