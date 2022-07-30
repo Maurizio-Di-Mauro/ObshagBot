3f5118e13590ebe503d3775a589e0229a84b3322
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2022-07-30 11:19:55 +0400

    Use Postgress syntax for queries

--- queries.py
+++ queries.py
@@ -5,21 +5,21 @@ def get_columns(columns_joined: str, table: str) -> str:
     return f"SELECT {columns_joined} FROM {table}"
 
 
-def get_list_of_expenses(return_amount: int) -> str:
+def get_list_of_expenses() -> str:
     return f"select e.id, e.amount, c.name " \
            f"from expenses e " \
            f"left join category c on " \
            f"c.codename=e.category_codename " \
-           f"order by created_time desc limit {return_amount}"
+           f"order by created_time desc limit %s"
 
 
 def get_today_expenses() -> str:
     return "select sum(amount) " \
            "from expenses " \
-           "where date(created_time)=date('now', 'localtime')"
+           "where created_time::date = now()::date"
 
 
-def get_expenses_starting_from(input_date: datetime) -> str:
-    return f"select sum(amount) " \
-           f"from expenses " \
-           f"where date(created_time) >= '{input_date}'"
+def get_expenses_starting_from() -> str:
+    return "select sum(amount) " \
+           "from expenses " \
+           "where created_time::date >= %s::date"
