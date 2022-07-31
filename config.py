2c166a2161e591c9660a1175a54b8732fbda8d90
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2022-07-31 10:49:53 +0400

    Use tuples for input values

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ class Config:
     TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
     TRUSTED_IDS = [int(i) for i in os.getenv('TRUSTED_IDS').split(" ")]
 
+    NUMBER_OF_LAST_EXPENSES: int = os.getenv('NUMBER_OF_LAST_EXPENSES') or 10
+
     # Database configuration
     DATABASE_URL = os.getenv("DATABASE_URL")
